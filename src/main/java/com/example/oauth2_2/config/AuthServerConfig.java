1e129ab11c19337e1bfdf07196dee4845d074936
h0l1da2 <whaleee@naver.com>
2023-01-24 19:41:31 +0900

    AuthenticationManager 를 빈으로 등록하고 AuthorizationServerConfigurerAdapter 의 configure 에 빈으로 등록된 AuthenticationManager 를 설정해서 사용자가 인증할 수 있게 한다

--- src/main/java/com/example/oauth2_2/config/AuthServerConfig.java
+++ src/main/java/com/example/oauth2_2/config/AuthServerConfig.java
@@ -1,10 +1,23 @@
 package com.example.oauth2_2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    // WebSecurityConfig 에서 빈으로 등록한 AuthenticationManager
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    // AuthenticationManager 설정하기
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
--- src/main/java/com/example/oauth2_2/config/WebSecurityConfig.java
+++ src/main/java/com/example/oauth2_2/config/WebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.oauth2_2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,7 +17,7 @@ import org.springframework.security.provisioning.UserDetailsManager;
  */
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -34,5 +36,11 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    // AuthenticationManager 빈으로 추가
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
