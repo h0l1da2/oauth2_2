fa8494ae9afc9e79d57fc61397a9eacb8be6551a
h0l1da2 <whaleee@naver.com>
2023-01-24 20:52:45 +0900

    클라이언트 자격 증명 그랜트 유형 클라이언트가 서버 상태를 알려주는 엔드포인트에 접근해야하는데 리소스 서버랑은 연관이 없으니까 사용자 인증 없이도 호출 가능하게 백엔드 끼리 인증하게 한다 - 단 , 사용자 자격 증명이 필요한 리소스에서는 @절대@ 접근 못하게 해야함 안 그러면 보안에 위험이 있겠죠 당연히 ?

--- src/main/java/com/example/oauth2_2/config/AuthServerConfigB.java
+++ src/main/java/com/example/oauth2_2/config/AuthServerConfigB.java
@@ -1,64 +1,64 @@
-package com.example.oauth2_2.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-
-/**
- * 암호 코드 그랜트 유형
- */
-@Configuration
-@EnableAuthorizationServer
-public class AuthServerConfigB extends AuthorizationServerConfigurerAdapter {
-
-    // WebSecurityConfig 에서 빈으로 등록한 AuthenticationManager
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    // AuthenticationManager 설정하기
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-    }
-
-    /**
-     * 암호 코드 그랜트 유형 설정
-     * redirectUri 를 설정해줘야 함
-     */
+//package com.example.oauth2_2.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+//
+///**
+// * 암호 코드 그랜트 유형
+// */
+//@Configuration
+//@EnableAuthorizationServer
+//public class AuthServerConfigB extends AuthorizationServerConfigurerAdapter {
+//
+//    // WebSecurityConfig 에서 빈으로 등록한 AuthenticationManager
+//    @Autowired
+//    private AuthenticationManager authenticationManager;
+//
+//    // AuthenticationManager 설정하기
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.authenticationManager(authenticationManager);
+//    }
+//
+//    /**
+//     * 암호 코드 그랜트 유형 설정
+//     * redirectUri 를 설정해줘야 함
+//     */
+////    @Override
+////    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+////        clients.inMemory()
+////                .withClient("client")
+////                .secret("secret")
+////                .authorizedGrantTypes("authorization_code")
+////                .scopes("read")
+////                .redirectUris("http://localhost:8080/home");
+////    }
+//
+//    /**
+//     * 클라이언트마다 그랜트 유형이 다르다면 ?
+//     */
 //    @Override
 //    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 //        clients.inMemory()
-//                .withClient("client")
-//                .secret("secret")
+//                .withClient("client1")
+//                .secret("secret1")
 //                .authorizedGrantTypes("authorization_code")
+//                .scopes("read")
+//                .redirectUris("http://localhost:8080/home")
+//
+//                .and()
+//                .withClient("client2")
+//                .secret("secret2")
+//                // 승인 코드, 암호, 갱신 토큰 허가 모두 이용 가능
+//                .authorizedGrantTypes("authorization_code", "password", "refresh_token")
 //                .scopes("read")
 //                .redirectUris("http://localhost:8080/home");
 //    }
-
-    /**
-     * 클라이언트마다 그랜트 유형이 다르다면 ?
-     */
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("client1")
-                .secret("secret1")
-                .authorizedGrantTypes("authorization_code")
-                .scopes("read")
-                .redirectUris("http://localhost:8080/home")
-
-                .and()
-                .withClient("client2")
-                .secret("secret2")
-                // 승인 코드, 암호, 갱신 토큰 허가 모두 이용 가능
-                .authorizedGrantTypes("authorization_code", "password", "refresh_token")
-                .scopes("read")
-                .redirectUris("http://localhost:8080/home");
-    }
-
-
-}
+//
+//
+//}
